8b039b16590efe1153d2102a8576844cf37feeff
Jonathan Hersch <jonathan@gridcure.com>
2018-12-25 08:30:40 -0700

    added requirements.txt file

---
+++ initialize.sh
@@ -0,0 +1,7 @@
+cd ~
+wget http://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
+bash Anaconda2-4.3.0-Linux-x86_64.sh
+source ~/.bashrc
+conda update conda
+conda install pip
+conda install virtualenv
